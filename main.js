a4a94be166859a099ca776dfac3bb464b1eaea51
Yuta Osawa <yutaosawa81@gmail.com>
2024-10-05 03:46:24 +0900

    feat: Implement simple worker class

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 import childprocess from 'child_process'
 import readline from 'readline'
+import { EventEmitter, once } from 'events'
 
-const worker = childprocess.spawn('go', ['run', 'main.go'])
+class Worker extends EventEmitter {
+  constructor() {
+    super()
+    this.process = childprocess.spawn('go', ['run', 'main.go'])
+    this.process.on('exit', () => {
+      this.emit('exit')
+    })
+
+    this.reader = readline.createInterface({
+      input: this.process.stdout,
+    })
+    this.reader.on('line', (line) => {
+      this.handleLine(line)
+    })
+  }
+
+  handleLine(line) {
+    this.emit('message', line)
+  }
+
+  async call(msg) {
+    this.process.stdin.write(msg + '\n')
+    const [reply] = await once(this, 'message')
+    return reply
+  }
+
+  exit() {
+    this.process.kill()
+  }
+}
+
+const worker = new Worker()
 
 worker.on('exit', () => {
   console.log('worker exit')
 })
 
-worker.stdin.write('hello\n')
-
-const reader = readline.createInterface({
-  input: worker.stdout,
-})
-
-reader.on('line', (line) => {
-  console.log(line)
-})
-
-reader.on('close', () => {
-  console.log('closed')
-})
+console.log(await worker.call('hello\n'))
