1641cb7972f2bab78be34fdcd9808b942acb8d46
Yuta Osawa <yutaosawa81@gmail.com>
2024-10-05 04:44:52 +0900

    feat: Identity each request with id and handling errors

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,12 +15,39 @@ func main() {
 
 	for {
 		scanner.Scan()
-		message := scanner.Text()
+		request := scanner.Text()
+
+		id, message, err := parseRequest(request)
+
+		if err != nil {
+			reply(writer, id, err.Error())
+			continue
+		}
 
 		switch message {
-		case "hello":
-			writer.Write([]byte("world\n"))
+		case "hello1":
+			reply(writer, id, "world1")
+		case "hello2":
+			reply(writer, id, "world2")
+		case "hello3":
+			reply(writer, id, "world3")
 		}
 
 	}
+}
+
+func parseRequest(s string) (int, string, error) {
+	parts := strings.SplitN(s, ",", 2)
+
+	id, err := strconv.Atoi(parts[0])
+
+	if len(parts) < 2 || err != nil {
+		return -id, "", fmt.Errorf("invalid request: \"%s\"", s)
+	}
+
+	return id, parts[1], nil
+}
+
+func reply(w io.Writer, id int, message string) {
+	fmt.Fprintf(w, "%d,%s\n", id, message)
 }
--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import childprocess from 'child_process'
 import readline from 'readline'
-import { EventEmitter, once } from 'events'
+import { EventEmitter, on } from 'events'
 
 class Worker extends EventEmitter {
   constructor() {
     super()
+
+    this._messageId = 1
+
     this.process = childprocess.spawn('go', ['run', 'main.go'])
+    this.process.stderr.pipe(process.stderr)
     this.process.on('exit', () => {
       this.emit('exit')
     })
@@ -19,13 +23,26 @@ class Worker extends EventEmitter {
   }
 
   handleLine(line) {
-    this.emit('message', line)
+    const sep = line.indexOf(',')
+    const [id, reply] = [line.slice(0, sep), line.slice(sep + 1)]
+    if (id == 0) {
+      console.error('error reply', line)
+      return
+    }
+    this.emit('_reply', id, reply)
   }
 
-  async call(msg) {
-    this.process.stdin.write(msg + '\n')
-    const [reply] = await once(this, 'message')
-    return reply
+  async call(message) {
+    if (message.includes('\n'))
+      throw new Error('message cannot contain newline')
+
+    const id = this._messageId++
+    this.process.stdin.write(id + ',' + message + '\n')
+
+    for await (const [replyId, reply] of on(this, '_reply')) {
+      if (replyId == id) return reply
+      if (replyId == -id) throw new Error(reply)
+    }
   }
 
   exit() {
@@ -39,4 +56,11 @@ worker.on('exit', () => {
   console.log('worker exit')
 })
 
-console.log(await worker.call('hello\n'))
+console.log(
+  'result',
+  await Promise.allSettled([
+    worker.call('hello1'),
+    worker.call('hello2'),
+    worker.call('hello3'),
+  ])
+)
